7e54458886a82555596f1883b9649e5ad9c189c0
Erick Gannem <erickgannem@gmail.com>
2020-12-11 08:54:14 -0400

    route for documents, single document and signer

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import ClickSignController from './controllers/ClickSignController.js';
 
 const routes = Router();
 
-routes.get('/get-documents', ClickSignController.getDocuments);
+routes.get('/documents', ClickSignController.getDocuments);
+routes.get('/documents/:document_key', ClickSignController.getDocument);
+routes.get('/signers/:signer_key', ClickSignController.getSigner);
 
 export default routes;
