509a580d1869d7cdf83c510c136ef824678caee8
Erick Gannem <erickgannem@gmail.com>
2020-12-28 14:27:44 -0400

    change redis url to production

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis'
 
-const cache = new Redis(6379, '127.0.0.1')
+const cache = new Redis(process.env.REDIS_URL)
 
 cache.on('connect', function () {
   process.stdout.write('\n>> [REDIS] Connected\n')
