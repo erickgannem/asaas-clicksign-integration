f66fe8d94704d118e279da131c5b24e6b729c293
Erick Gannem <erickgannem@gmail.com>
2020-12-29 11:41:33 -0400

    remove build directory and add to .gitignore

--- build/helpers/removeCPFchars.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function removeCPFChars(cpf) {
-    return cpf.split('.').join('').split('-').join('');
-}
-exports.default = removeCPFChars;
--- build/middlewares/errorMiddleware.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var errorMiddleware = function (err, req, res, next) { return res.status(500).json({ error: err.message }); };
-exports.default = errorMiddleware;
