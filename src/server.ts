9470e186384fddf89bab102de7c2c54acf83fc1b
Erick Gannem <erickgannem@gmail.com>
2020-12-29 11:48:24 -0400

    edit server feedback

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import server from './app'
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => process.stdout.write(`Server is listening on port: ${PORT} \n`))
+server.listen(PORT, () => process.stdout.write(`\n>> [Server] Running on port: ${PORT}`))
