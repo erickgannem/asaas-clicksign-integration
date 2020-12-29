d4ecfe16d65b9c80c86991d78820c6ad843fae5c
Erick Gannem <erickgannem@gmail.com>
2020-12-28 20:54:59 -0400

    cache configuration

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -5,10 +5,17 @@ const { REDIS_URL } = process.env
 const cache = new Redis(REDIS_URL)
 
 cache.on('connect', function () {
-  process.stdout.write('\n>> [REDIS] Connected\n')
+  process.stdout.write('\n>> [REDIS] Client is CONNECTED\n')
+})
+cache.on('ready', function () {
+  process.stdout.write('\n>> [REDIS] Client is READY\n')
+})
+cache.on('reconnecting', function () {
+  process.stdout.write('\n>> [REDIS] Client is RECONNECTING\n')
 })
 cache.on('error', function () {
-  process.stdout.write('\n>> [REDIS] Can\'t connect. Please restart the server.\n')
+  process.stdout.write('\n>> [REDIS] Client can\'t connect. Please restart the server.\n')
+  cache.connect()
 })
 
 export default cache
