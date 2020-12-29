492bae7266609243da69165a22b0aee28542fa4e
Erick Gannem <erickgannem@gmail.com>
2020-12-28 20:49:24 -0400

    env var edit

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,13 +1,14 @@
 import Redis from 'ioredis'
 
-const cache = new Redis(process.env.REDIS_URL)
+const { REDIS_URL } = process.env
+
+const cache = new Redis(REDIS_URL)
 
 cache.on('connect', function () {
   process.stdout.write('\n>> [REDIS] Connected\n')
 })
 cache.on('error', function () {
   process.stdout.write('\n>> [REDIS] Can\'t connect. Please restart the server.\n')
-  cache.quit()
 })
 
 export default cache
