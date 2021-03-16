3266861e40385879b3c172da4728c1f822248e3e
Erick Gannem <erickgannem@gmail.com>
2021-03-16 13:43:02 -0400

    Schema

--- src/database/models/Payment.ts
+++ src/database/models/Payment.ts
@@ -8,5 +8,39 @@ const paymentSchema = new Schema({
     required: true
   },
   paymentData: {
+    type: {
+      event: String,
+      payment: {
+        type: {
+          object: String,
+          id: String,
+          dateCreated: String,
+          customer: String,
+          subscription: {
+            type: String,
+            required: false
+          },
+          installment: {
+            type: String,
+            required: false
+          },
+          dueDate: String,
+          value: Number,
+          netValue: Number,
+          billingType: String,
+          status: String,
+          description: String,
+          externalReference: String,
+          confirmedDate: String
+          // ...
+        },
+        required: true
+      }
+    },
+    required: true
   }
 })
+
+const Payment = model('Payment', paymentSchema)
+
+export default Payment
