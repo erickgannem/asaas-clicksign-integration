8a8d13cfa6a9024c9f26da34b6eade21e98e6277
Erick Gannem <erickgannem@gmail.com>
2021-03-17 14:06:25 -0400

    prepare invoice handler

--- src/controllers/AsaasController.ts
+++ src/controllers/AsaasController.ts
@@ -115,49 +115,19 @@ export default class AsaasController {
   }
 
   static async createInvoice (req: Request, res: Response, next: NextFunction) {
-    const body = {
-      event: 'PAYMENT_RECEIVED',
-      payment: {
-        object: 'payment',
-        id: 'pay_080225913252',
-        dateCreated: '2017-03-10',
-        customer: 'cus_G7Dvo4iphUNk',
-        subscription: 'sub_VXJBYgP2u0eO', // somente quando pertencer a uma assinatura
-        installment: 'ins_000000001031', // somente quando pertencer a um parcelamento
-        dueDate: '2017-03-15',
-        value: 100.00,
-        netValue: 94.51,
-        billingType: 'CREDIT_CARD',
-        status: 'RECEIVED',
-        description: 'Pedido 056984',
-        externalReference: '056984',
-        confirmedDate: '2017-03-15',
-        originalValue: null,
-        interestValue: null,
-        originalDueDate: '2017-06-10',
-        paymentDate: null,
-        clientPaymentDate: null,
-        invoiceUrl: 'https://www.asaas.com/i/080225913252',
-        bankSlipUrl: null,
-        invoiceNumber: '00005101',
-        deleted: false,
-        creditCard: {
-          creditCardNumber: '8829',
-          creditCardBrand: 'MASTERCARD',
-          creditCardToken: 'a75a1d98-c52d-4a6b-a413-71e00b193c99'
-        }
-      }
-    }
-
+    const { body } = req
     const { event } = body
+
     const PAYMENT_RECEIVED = 'PAYMENT_RECEIVED'
 
-    if (event === PAYMENT_RECEIVED) {
-      // test
-      const payment = await db.Payment.create({ paymentData: body })
-      console.log(payment)
+    try {
+      if (event === PAYMENT_RECEIVED) {
+        const payment = await db.Payment.create(body)
+        return res.status(200).json({ message: 'payment received' + payment })
+      }
+      return
+    } catch (err) {
+      return next(err)
     }
-
-    return res.status(200).json({ message: '/invoices hitted succesfully' })
   }
 }
